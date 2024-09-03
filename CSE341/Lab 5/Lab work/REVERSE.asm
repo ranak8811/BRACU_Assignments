994cb0c19f320c83caf93e5939f07fc9e9293e4a
MD. ANWAR HOSSAIN <104732602+ranak8811@users.noreply.github.com>
2024-09-03 19:53:49 +0600

    correct reverse code

--- CSE341/Lab 5/Lab work/REVERSE.asm
+++ CSE341/Lab 5/Lab work/REVERSE.asm
@@ -1,26 +1,34 @@
 .MODEL SMALL
-
 .STACK 100H
 
 .DATA
-
 NAME1 DB "NOWSHIN"
 
 .CODE
-
 MAIN PROC
-    
     MOV AX, @DATA
     MOV DS, AX
-    
-    MOV SI, 0
-    MOV CX, 7
-    
+
+    MOV SI, OFFSET NAME1
+    MOV CX, 7          ; Length of the string
+
+    ; Push characters onto the stack
     PUSH_LOOP:
-    
-    
-    
-    
-    
-    MAIN ENDP
+    MOV AL, [SI]
+    PUSH AX
+    INC SI
+    LOOP PUSH_LOOP
+
+    ; Pop characters and display them in reverse order
+    POP_LOOP:
+    POP AX
+    MOV DL, AL         ; Move the character to DL for printing
+    MOV AH, 2          ; Function to display character
+    INT 21H
+    LOOP POP_LOOP
+
+    MOV AH, 4CH        ; Exit to DOS
+    INT 21H
+
+MAIN ENDP
 END MAIN
