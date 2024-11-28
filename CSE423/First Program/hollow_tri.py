e38abf08daf866226603c2cb749ffd275063bc75
MD. ANWAR HOSSAIN <104732602+ranak8811@users.noreply.github.com>
2024-11-28 23:10:38 +0600

    Add files via upload

---
+++ CSE423/First Program/hollow_tri.py
@@ -0,0 +1,43 @@
+from OpenGL.GL import *
+from OpenGL.GLUT import *
+from OpenGL.GLU import *
+
+
+def draw_points(x, y):
+    # glPointSize(5) #pixel size. by default 1 thake
+    glLineWidth(15)
+    glBegin(GL_LINE_LOOP)
+    glVertex2f(x,y) #jekhane show korbe pixel
+    glColor3f(1.0, 0.0,0.0)
+    glVertex2f(450,250)
+    glVertex2f(350,450)
+    glEnd()
+
+
+def iterate():
+    glViewport(0, 0, 500, 500)
+    glMatrixMode(GL_PROJECTION)
+    glLoadIdentity()
+    glOrtho(0.0, 500, 0.0, 500, 0.0, 1.0)
+    glMatrixMode (GL_MODELVIEW)
+    glLoadIdentity()
+
+def showScreen():
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+    glLoadIdentity()
+    iterate()
+    glColor3f(1.0, 1.0, 0.0) #konokichur color set (RGB)
+    #call the draw methods here
+    draw_points(250, 250)
+    glutSwapBuffers()
+
+
+
+glutInit()
+glutInitDisplayMode(GLUT_RGBA)
+glutInitWindowSize(500, 500) #window size
+glutInitWindowPosition(0, 0)
+wind = glutCreateWindow(b"OpenGL Coding Practice") #window name
+glutDisplayFunc(showScreen)
+
+glutMainLoop()
