e572e970ef33fc52b3d2a42c544ee0df4a64513f
MD. ANWAR HOSSAIN <104732602+ranak8811@users.noreply.github.com>
2025-12-13 19:21:53 +0600

    lab 3 files

---
+++ CSE420/Lab 3/script.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+yacc -d -y --debug --verbose 22101710.y
+echo 'Generated the parser C file as well the header file'
+g++ -w -c -o y.o y.tab.c
+echo 'Generated the parser object file'
+flex 22101710.l
+echo 'Generated the scanner C file'
+g++ -fpermissive -w -c -o l.o lex.yy.c
+# if the above command doesn't work try g++ -fpermissive -w -c -o l.o lex.yy.c
+echo 'Generated the scanner object file'
+g++ y.o l.o
+echo 'All ready, running'
+./a.out 22101710/input2.c > 22101710_log.txt
+echo 'logfile'
+echo 'errorfile'
+cat 22101710_log.txt
